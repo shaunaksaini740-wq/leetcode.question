df7b9c5280fe6d250d40ca4e61dc48b3716cad9a
Shaunak saini <shaunaksaini740@gmail.com>
2026-02-21 00:28:37 +0530

    Create 263.c

---
+++ 263.c
@@ -0,0 +1,10 @@
+bool isUgly(int n) {
+    if (n == 0)
+        return false;
+    
+    for (; n % 2 == 0; n = n / 2);
+    for (; n % 3 == 0; n = n / 3);
+    for (; n % 5 == 0; n = n / 5);
+    
+    return n == 1;
+}
