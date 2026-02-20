5c2a29b4bb55a4443136ba8cf9c039ffea0daa9d
Shaunak saini <shaunaksaini740@gmail.com>
2026-02-21 00:42:46 +0530

    Create 693.java

---
+++ 693.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public boolean hasAlternatingBits(int n) 
+    {
+        int prevBit= n & 1;
+        n=n >> 1;
+        while (n > 0) 
+        {
+            int currBit = n & 1;
+            if (currBit==prevBit) 
+            {
+                return false;
+            }
+            prevBit=currBit;
+            n= n >> 1;
+        }
+        return true;
+    }
+}
